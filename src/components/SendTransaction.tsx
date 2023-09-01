5bcaf5026fd84682d8d9d2391840e57cd8585289
karooolis <hello@karolisram.com>
2023-09-01 12:47:03 +0300

    chore: 🔧 cleanup

--- src/components/SendTransaction.tsx
+++ src/components/SendTransaction.tsx
@@ -1,11 +1,9 @@
-import { parseEther } from "viem";
-import { useSendTransaction, useNetwork, useWalletClient } from "wagmi";
+import { useWalletClient } from "wagmi";
 
 import Contract from "../../contracts/artifacts/contracts/MockERC20.sol/MyToken.json";
 
 export function SendTransaction() {
   const account = useWalletClient();
-
 
   async function sendTransaction() {
     await account.data?.deployContract({
