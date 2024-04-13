bf7c3ca651a4819ed8a54e7abba4a329400d13c2
karooolis <hello@karolisram.com>
2024-04-13 19:42:27 +0300

    chore: 🔧 remove redundant files

--- src/pages/api/deploy.ts
+++ src/pages/api/deploy.ts
@@ -1,72 +1,14 @@
-import fs from "fs";
-import path from "path";
 import solc from "solc";
 import _ from "lodash";
-
 import type { NextApiRequest, NextApiResponse } from "next";
+import { findFiles } from "./helpers/findFiles";
 
+// TODO: add proper output types
 type ResponseData = {
-  message: string;
-};
-
-const isRelativePath = (path: string) => {
-  return path.startsWith(".");
-};
-
-const findFiles = (
-  contractCode: string,
-  importContextPath: string,
-  imports: Array<{ path: string; content: string }> = []
-): Array<{ path: string; content: string }> => {
-  const fullImportsRegex = /import\s+(?:{.*?}\s+from\s+)?['"](.*?)['"];/g;
-  const newFullImports = contractCode.match(fullImportsRegex);
-
-  if (!newFullImports) return [...imports];
-
-  const newlyFoundImports = [];
-
-  for (let i = 0; i < newFullImports.length; i++) {
-    const importFullStatement = newFullImports[i];
-
-    const importPathRegex = /import\s+(?:{.*?}\s+from\s+)?['"](.*?)['"]/;
-    const importPathMatch = importFullStatement.match(importPathRegex);
-
-    // if (!importPathMatch) continue;
-    const importPath = importPathMatch[1];
-
-    const isRelative = isRelativePath(importPath);
-    const importContextPathWithoutSol = importContextPath.substring(
-      0,
-      importContextPath.lastIndexOf("/")
-    );
-
-    const resolvedPath = isRelative
-      ? path.join(importContextPathWithoutSol, importPath)
-      : importPath;
-
-    const finalResolvedPath = path.join(
-      process.cwd(),
-      "src/lib",
-      resolvedPath
-    );
-
-    imports.push({
-      path: resolvedPath,
-      content: fs.readFileSync(finalResolvedPath, "utf8"),
-    });
-
-    newlyFoundImports.push({
-      path: resolvedPath,
-      content: fs.readFileSync(finalResolvedPath, "utf8"),
-    });
-  }
-
-  return _.flatten([
-    ...imports,
-    ...newlyFoundImports.map((i) => {
-      return findFiles(i.content, i.path, imports);
-    }),
-  ]);
+  abi: any;
+  bytecode: any;
+  input: any;
+  output: any;
 };
 
 export default function handler(
