2342b0ca407f932d29b5a239a4937cf3115451e0
karooolis <hello@karolisram.com>
2024-06-08 22:38:30 +0400

    fix: 🐛 file transformer for sol files

--- src/__tests__/compile.test.tsx
+++ src/__tests__/compile.test.tsx
@@ -1,48 +1,52 @@
-import { expect, test } from "vitest";
+// import { expect, test } from "vitest";
+import "@testing-library/jest-dom";
 import { compile } from "../pages/api/helpers/compile";
 import { constructForm } from "../components/ContractOptionsForm/ContractOptionsForm";
 import { SCHEMAS_MAP } from "../components/ContractOptionsForm/constants";
 import { getTemplate } from "../utils/templates";
 
-test("Compile", async () => {
-  const contractType = "erc20";
-  const library = "openzeppelin";
-  const formSchema = SCHEMAS_MAP[contractType];
-  const values = {
-    baseURI: "",
-    name: "MyToken",
-    symbol: "TKN",
-    premint: 1000000,
-    features: [],
-    accessControl: "none",
-    upgradeability: "none",
-    verify: true,
-    license: "MIT",
-    pragma: "^0.8.25",
-  };
 
-  const template = getTemplate(values, contractType, library);
-  // console.log(template);
+describe("Page", () => {
+  it("renders a heading", () => {
+    const contractType = "erc721";
+    const library = "openzeppelin";
+    const formSchema = SCHEMAS_MAP[contractType];
+    const values = {
+      baseURI: "",
+      name: "MyToken",
+      symbol: "TKN",
+      premint: 1000000,
+      features: [],
+      accessControl: "none",
+      upgradeability: "none",
+      verify: true,
+      license: "MIT",
+      pragma: "^0.8.25",
+    };
 
-//   const name = "MyToken";
-//   const contract = `
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.25;
+    const template = getTemplate(values, contractType, library);
+    console.log("template res", template);
 
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+    //   const name = "MyToken";
+    //   const contract = `
+    // // SPDX-License-Identifier: MIT
+    // pragma solidity ^0.8.25;
 
-// contract MyToken is ERC20 {
-//     constructor() ERC20("MyToken", "TKN") {
-//         _mint(msg.sender, 1000000 * 10 ** decimals());
-//     }
+    // import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-//     function mint(address to, uint256 amount) public {
-//         _mint(to, amount);
-//     }
-// }
-//   `;
+    // contract MyToken is ERC20 {
+    //     constructor() ERC20("MyToken", "TKN") {
+    //         _mint(msg.sender, 1000000 * 10 ** decimals());
+    //     }
 
-//   const result = compile(name, contract);
+    //     function mint(address to, uint256 amount) public {
+    //         _mint(to, amount);
+    //     }
+    // }
+    //   `;
 
-//   expect(result.output.errors).toBe(undefined);
+    //   const result = compile(name, contract);
+
+    //   expect(result.output.errors).toBe(undefined);
+  });
 });
